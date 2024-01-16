e160c5f24e75e6bfe424a8f4b4332c8aa1ee5b51
Sraddheya <ggsraddheya@gmail.com>
2024-01-16 17:30:23 +0000

    added static map background

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apiUrlAirQuality = 'https://airquality.googleapis.com/v1/currentConditions:lookup?';
 const apiUrlGeocoding = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const apiUrlStaticMap = 'https://maps.googleapis.com/maps/api/staticmap?'
 const apiKey = YOURKEY;
 
 let airInfo = {
@@ -40,6 +41,7 @@ async function getGeocodeData() {
         console.log(airInfo.location)
         document.querySelector('.location-text').innerText = address
         fetchAirQuality();
+        setMapBackground();
       }
   
     } catch (error) {
@@ -112,4 +114,28 @@ function setProgressBar(){
       clearInterval(progress);
     }
   }, speed);
+}
+
+//Limations on google maps sizes
+async function setMapBackground(){
+  try {
+    //document.body.style.backgroundImage = `url('${apiUrlStaticMap}center=${airInfo.location.latitude}, ${airInfo.location.longitude}&zoom=9&size=${window.innerWidth}x${window.innerHeight}&scale=2&key=${apiKey}')`;
+    //style=feature:road.highway%7Celement:geometry%7Cvisibility:simplified%7Ccolor:0xc280e9&style=feature:transit.line%7Cvisibility:simplified%7Ccolor:0xbababa&style=feature:road.highway%7Celement:labels.text.stroke%7Cvisibility:on%7Ccolor:0xb06eba&style=feature:road.highway%7Celement:labels.text.fill%7Cvisibility:on%7Ccolor:0xffffff
+    // [
+    //   {
+    //     "featureType": "all",
+    //     "stylers": [
+    //       { "color": "#C0C0C0" }
+    //     ]
+    
+    //style=featureType:all%7Ccolor:#C0C0C0
+    
+    //https://maps.googleapis.com/maps/api/staticmap?key=YOUR_API_KEY&center=47.65,-122.35&zoom=12&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.business%7Cvisibility:off&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text%7Cvisibility:off&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels%7Cvisibility:off&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels%7Cvisibility:off&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Cvisibility:off&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=480x360
+   
+    style = "element:geometry%7Ccolor:0x212121&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x757575&style=element:labels.text.stroke%7Ccolor:0x212121&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:administrative.neighborhood%7Cvisibility:off&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d";
+    document.body.style.backgroundImage = `url('${apiUrlStaticMap}center=${airInfo.location.latitude}, ${airInfo.location.longitude}&zoom=10&size=640x340&scale=2&style=${style}&key=${apiKey}')`;
+    console.log(window.innerWidth + " " + window.innerHeight)
+  } catch (error) {
+    console.error('Error setting map background', error);
+  }
 }
